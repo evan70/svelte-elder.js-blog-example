f25d02c66f4adf4d6219582960377546edd53225
evan70 <ivanpaldan@yandex.com>
2021-08-19 22:12:56 +0200

    initial commit

--- elder.config.js
+++ elder.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  origin: 'http://localhost:3000', // TODO: update this.
-  lang: 'sk',
+  origin: '', // TODO: update this.
+  lang: 'en',
   srcDir: 'src',
   distDir: 'public',
   rootDir: process.cwd(),
@@ -20,20 +20,38 @@ module.exports = {
   plugins: {
     '@elderjs/plugin-markdown': {
       routes: ['blog'],
+      contents: {
+        blog: 'contents/blog',
+      },
       useSyntaxHighlighting: {
-        theme: 'nord' // available themes: https://github.com/shikijs/shiki/blob/master/packages/themes/README.md#literal-values - try material-theme-darker
+        theme: 'nord', // available themes: https://github.com/shikijs/shiki/blob/master/packages/themes/README.md#literal-values - try material-theme-darker
         // theme is the only option available - for now.
       },
-      useTableOfContents: true // adds tocTree to each route's frontmatter data object.
+      slugFormatter: function (relativeFilePath, frontmatter) {
+        // custom slug - use blog title as slug
+        // default is markdown file name
+        let slug = frontmatter.title
+          .replace(/[^a-zA-Z ]/g, '')
+          .replace(/\s+/g, '-')
+          .toLowerCase();
+        return slug;
+      },
+      useElderJsPluginImages: false,
+      useTableOfContents: true,
+    },
+    'elderjs-plugin-blog-excerpt': {
+      routes: ['blog'],
+      overrideExcerpt: true,
+    },
+    'elderjs-plugin-blog-pagination': {
+      routes: ['blog'], // change to your blog route same as plugin-markdown route
+      postPerPage: 5, // change to your preferred post per apge
+      indexTemplate: 'BlogIndex', // change to your own Index.svelte template but without `.svelte`
     },
     '@elderjs/plugin-browser-reload': {
       // this reloads your browser when nodemon restarts your server.
       port: 8080,
     },
-    // '@elderjs/plugin-seo-check': {
-    //   display: ['errors', 'warnings'], // If the errors are too verbose remove 'warnings'
-    //   //writeLocation: './report.json', // if you want to write a report of errors
-    // },
   },
   shortcodes: { closePattern: '}}', openPattern: '{{' },
 };
--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ const sveltePreprocess = require('svelte-preprocess');
 module.exports = {
   preprocess: [
     sveltePreprocess({
-      sass: true,
       scss: {},
       postcss: {
         plugins: [require('autoprefixer')],
